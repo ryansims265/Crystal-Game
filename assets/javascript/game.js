8b18023d743fb5bfaaa6fc7c3bb169ea4678ed25
ryansims265 <ryansims265@gmail.com>
2019-06-25 20:46:46 -0400

    Fixed JS loop and displayed values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,3 @@
-$(document).ready(function() {
-
-//Define intial variables 
 var userscoreintial = 0;
 var winsintial = 0;
 var lossesintial = 0;
@@ -9,53 +6,107 @@ var lossesintial = 0;
 var min = 1;
 var max = 99; 
 var randomnum = Math.floor(Math.random() * 120 + 19);
-console.log(randomnum);
+console.log("Target Score is " + randomnum);
+
+$("#random-number").text("Target Score: " + randomnum);
+$("#winnumber").text("Wins: " + winsintial);
+$("#lossnumber").text("Losses: " + lossesintial);
+$("#user-score").text("Your score is: " + userscoreintial);
+
 
 //Set up each jewel variables 
-var j1 = Math.floor(Math.random() * 11);
-var j2 = Math.floor(Math.random() * 11);
-var j3 = Math.floor(Math.random() * 11);
-var j4 = Math.floor(Math.random() * 11);
+var j1 = Math.floor(Math.random() * 11 + 1);
+var j2 = Math.floor(Math.random() * 11 + 1);
+var j3 = Math.floor(Math.random() * 11 + 1);
+var j4 = Math.floor(Math.random() * 11 + 1);
+
+console.log("J1 is " + j1);
+console.log("J2 is " + j2);
+console.log("J3 is " + j3);
+console.log("J4 is " + j4);
+
 
 //Random values are assigned to each of the gem gifs and listen for the on-click event 
 function reset() {
     randomnum = [Math.floor(Math.random() * 120) + 19];
-    j1 = Math.floor(Math.random() * 11);
-    j2 = Math.floor(Math.random() * 11);
-    j3 = Math.floor(Math.random() * 11);
-    j4 = Math.floor(Math.random() * 11);
+    j1 = Math.floor(Math.random() * 11 + 1);
+    j2 = Math.floor(Math.random() * 11 + 1);
+    j3 = Math.floor(Math.random() * 11 + 1);
+    j4 = Math.floor(Math.random() * 11 + 1);
+    userscoreintial = 0;
+    $("#random-number").text("Target Score: " + randomnum);
+    $("#winnumber").text("Wins: " + winsintial);
+    $("#lossnumber").text("Losses: " + lossesintial);
+    $("#user-score").text("Your score is: " + userscoreintial);
+    console.log("J1 is " + j1);
+    console.log("J2 is " + j2);
+    console.log("J3 is " + j3);
+    console.log("J4 is " + j4);
+
 }
 
-document.getElementById("gif1").onclick=function (){
-    console.log("Gif 1 on-click works");
+function win (){
+    winsintial++;
+    alert("You Won!");
+    $("#winnumber").text("wins: " + winsintial);
+    reset();
 }
 
-document.getElementById("gif2").onclick=function (){
-    console.log("Gif 2 on-click works");
-}
-
-document.getElementById("gif3").onclick=function (){
-    console.log("Gif 3 on-click works");
-}
-
-document.getElementById("gif4").onclick=function (){
-    console.log("Gif 4 on-click works");
+function lose (){
+    lossesintial++;
+    alert("You Lost!");
+    $("#lossnumber").text("Losses: " + lossesintial);
+    reset();
 }
 
 
+$("#gif1").click(function () {
+        userscoreintial = userscoreintial + j1;
+        console.log("Your score is: " + userscoreintial);
+        $("#user-score").text("Your score: " + userscoreintial);
+        if (userscoreintial == randomnum) {
+            win();
+        }
+        else if (userscoreintial > randomnum) {
+            lose();
+        }
+    });
 
+$("#gif2").click(function () {
+        userscoreintial = userscoreintial + j2;
+        console.log("Your score is: " + userscoreintial);
+        $("#user-score").text("Your score: " + userscoreintial);
 
-//When each gif is clicked the value is added to the user score
+        if (userscoreintial == randomnum) {
+            win();
+        }
+        else if (userscoreintial > randomnum) {
+            lose();
+        }
 
-//When the user score equals the computerscore, the user wins, 1 is added to the wins, the user and ckomputer score both reset. The user score goes to 0 and the computer selects a new random nunber 
-//New random values are assigned to the gifs each time 
-
-document.getElementById("gif1").v
-
-document.getElementById("user-score").textContent = "Your score: " + userscoreintial;
-document.getElementById("random-number").textContent = "Target Score: " + randomnum;
-
-document.getElementById("winnumber").textContent = "Wins: " + winsintial;
-document.getElementById("lossnumber").textContent = "Losses: " + lossesintial; 
-
+$("#gif3").click(function () {
+            userscoreintial = userscoreintial + j3;
+            console.log("Your score is: " + userscoreintial);
+            $("#user-score").text("Your score: " + userscoreintial);
+    
+            if (userscoreintial == randomnum) {
+                win();
+            }
+            else if (userscoreintial > randomnum) {
+                lose();
+            }
 });
+
+$("#gif4").click(function () {
+            userscoreintial = userscoreintial + j4;
+            console.log("Your score is: " + userscoreintial);
+            $("#user-score").text("Your score: " + userscoreintial);
+    
+            if (userscoreintial == randomnum) {
+                win();
+            }
+            else if (userscoreintial > randomnum) {
+                lose();
+            }
+        });
+    });
